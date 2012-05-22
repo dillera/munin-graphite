8f308c09b0a0970cd9d6b676dc6ab09ac389c311
Andy Diller <dillera@gmail.com>
2012-05-22 17:13:20 -0400

    modified to remove loop, cron will execute once a min

--- munin-graphite.rb
+++ munin-graphite.rb
@@ -23,6 +23,11 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 # 
 
+# add this to /etc/cron.d
+#   # ensure the munin-graphite is invoked everymin
+#   */1 * * * * dillera /opt/monitor/munin-graphite/munin-graphite.rb >/dev/null 2>&1
+
+
 require 'socket'
 
 class Munin
@@ -67,43 +72,41 @@ class Carbon
   end
 end
 
-while true
-  metric_base = "servers."
-  all_metrics = Array.new
 
-  munin = Munin.new(ARGV[0])
-  munin.get_response("nodes").each do |node|
-    metric_base << node.split(".").reverse.join(".")
-    puts "Doing #{metric_base}"
-    munin.get_response("list")[0].split(" ").each do |metric|
-      puts "Grabbing #{metric}"
-      mname = "#{metric_base}"
-      has_category = false
-      base = false
-      munin.get_response("config #{metric}").each do |configline|
-        if configline =~ /graph_category (.+)/
-          mname << ".#{$1}"
-          has_category = true
-        end
-        if configline =~ /graph_args.+--base (\d+)/
-          base = $1
-        end
+metric_base = "servers."
+all_metrics = Array.new
+
+munin = Munin.new(ARGV[0])
+munin.get_response("nodes").each do |node|
+  metric_base << node.split(".").reverse.join(".")
+  puts "Doing #{metric_base}"
+  munin.get_response("list")[0].split(" ").each do |metric|
+    puts "Grabbing #{metric}"
+    mname = "#{metric_base}"
+    has_category = false
+    base = false
+    munin.get_response("config #{metric}").each do |configline|
+      if configline =~ /graph_category (.+)/
+        mname << ".#{$1}"
+        has_category = true
       end
-      mname << ".other" unless has_category
-      munin.get_response("fetch #{metric}").each do |line|
-        line =~ /^(.+)\.value\s+(.+)$/
-        field = $1
-        value = $2
-        all_metrics << "#{mname}.#{metric}.#{field} #{value} #{Time.now.to_i}"
+      if configline =~ /graph_args.+--base (\d+)/
+        base = $1
       end
     end
+    mname << ".other" unless has_category
+    munin.get_response("fetch #{metric}").each do |line|
+      line =~ /^(.+)\.value\s+(.+)$/
+      field = $1
+      value = $2
+      all_metrics << "#{mname}.#{metric}.#{field} #{value} #{Time.now.to_i}"
+    end
   end
+end
 
-  carbon = Carbon.new(ARGV[1])
-  all_metrics.each do |m|
-    puts "Sending #{m}"
-    carbon.send(m)
-  end
-  sleep 60
+carbon = Carbon.new(ARGV[1])
+all_metrics.each do |m|
+  puts "Sending #{m}"
+  carbon.send(m)
 end
 
